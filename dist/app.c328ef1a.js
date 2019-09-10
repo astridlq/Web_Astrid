0091acb49d4f2711b4775511c08a45c2bd559bd0
Astrid Le Quere <astrid.lequere@gmail.com>
2019-09-10 11:15:06 +0200

    changed the content overflow on the main page

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -810,7 +810,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64952" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51142" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
